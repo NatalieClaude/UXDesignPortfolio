f0f8020563e117e201c89459c27205a777a0b878
NatalieClaude <natalieclaude@gmail.com>
2024-11-25 22:53:50 +0000

    updates

--- main.js
+++ main.js
@@ -21,3 +21,8 @@ document.querySelectorAll("a").forEach((link) => {
     }
   });
 });
+
+// Dynamically load the footer
+  fetch('footer.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('footer-placeholder').innerHTML = data);
